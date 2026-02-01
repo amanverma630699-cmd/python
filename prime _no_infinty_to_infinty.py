e535f5b034377e424f45e539eae93e57e172afb1
AMAN VERMA <amanverma630699@gmail.com>
2026-02-01 12:13:44 +0530

    Fix variable naming in prime number identification
    Correct variable name from 'Nreminder' to 'No_reminder' for consistency.

--- prime _no_infinty_to_infinty.py
+++ prime _no_infinty_to_infinty.py
@@ -4,7 +4,7 @@ end = int(input("Enter ending number: "))
 
 for num in range(start, end + 1):
     if num>1:
-        Nreminder=0
+        No_reminder=0 
         for i in range(1,num+1):
             if num%i==0:
                 Nreminder+=1
