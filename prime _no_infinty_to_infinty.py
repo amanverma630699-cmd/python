e4d31113f1074cc7839c9618ed51d7d59278c056
AMAN VERMA <amanverma630699@gmail.com>
2026-02-01 12:14:20 +0530

    Fix variable name typo in prime number checker

--- prime _no_infinty_to_infinty.py
+++ prime _no_infinty_to_infinty.py
@@ -8,5 +8,6 @@ for num in range(start, end + 1):
         for i in range(1,num+1):
             if num%i==0:
                 Nreminder+=1
-        if Nreminder==2:
+        if No_reminder==2:
             print(num,end="  ")
+
