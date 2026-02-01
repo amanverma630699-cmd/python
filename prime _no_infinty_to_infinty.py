31f08f64e7c4faa40ad44e21dd106c0d346b85c1
AMAN VERMA <amanverma630699@gmail.com>
2026-02-01 12:15:38 +0530

    Fix indentation and formatting issues in prime number script

--- prime _no_infinty_to_infinty.py
+++ prime _no_infinty_to_infinty.py
@@ -7,7 +7,6 @@ for num in range(start, end + 1):
         No_reminder=0 
         for i in range(1,num+1):
             if num%i==0:
-                Nreminder+=1
+                No_reminder+=1
         if No_reminder==2:
             print(num,end="  ")
-
